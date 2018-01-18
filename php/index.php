2fc42972d5c6ee81831daa3b749c51085e211d31
Link1986 <ali.ghoudi@gmail.com>
2018-01-18 22:45:02 +0100

    Séparation du code en deux pages

--- php/index.php
+++ php/index.php
@@ -10,60 +10,8 @@
             die('Erreur : '.$e->getMessage());
         }
 
-        $reponse = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY id DESC LIMIT 0,5');
+        $reponse = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+        require('affichageAccueil.php');
         
 ?>
-
-
-<!DOCTYPE html>
-
-<html lang="fr">
-
-    <head>
-    
-        <meta charset="UTF-8">
-        <title>Projet de Blog</title>
-        <link rel="stylesheet" href="css/blog.css">
-        
-    </head>
-    
-    <body>
-    
-        <h1>Mon super blog !</h1>
-        
-        <p>Derniers billets du blog:</p>
-        
-        <?php
-            while ($donnees = $reponse->fetch())
-            {
-        ?>
-        
-        <div class="news">
-        
-            <h3>
-            <?php echo htmlspecialchars($donnees['titre']); ?>
-            <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-            </h3>
-    
-        <p>
-        
-        <?php
-    
-            echo nl2br(htmlspecialchars($donnees['contenu']));
-            
-        ?>
-        
-        <br/>
-        <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
-        </p>
-        
-        </div>
-        
-        <?php
-            } 
-        $reponse->closeCursor();
-        ?>
-    
-    </body>
-    
-</html>
