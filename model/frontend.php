bee23703ac01e082660fc259227423f7b7ada9d7
Link1986 <ali.ghoudi@gmail.com>
2018-01-22 17:55:47 +0100

    fichier déplacé

--- model/frontend.php
+++ model/frontend.php
@@ -32,6 +32,18 @@
         return $comments;
     }
 
+    function postComment($postId, $author, $comment)
+        
+    {
+        
+        $db = dbConnect();
+        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+        return $affectedLines;
+        
+    }
+
     function dbConnect()
     
     {
