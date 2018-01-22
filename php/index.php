8538e53ad39fed2a1113147da6d47a86fd27cfae
Link1986 <ali.ghoudi@gmail.com>
2018-01-22 15:17:17 +0100

    modification du fichier index

--- php/index.php
+++ php/index.php
@@ -1,8 +1,31 @@
 <?php
 
-    require('model.php');
+    require('controller.php');
 
-    $posts = getPosts();
+    if (isset($_GET['action'])) {
+        
+        if ($_GET['action'] == 'listPosts') {
+            
+            listPosts();
+            
+        }
+        
+        elseif ($_GET['action'] == 'post') {
+            
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                
+                post();
+            }
+            
+            else {
+                
+                echo 'Erreur : aucun identifiant de billet envoyé';
+            }
+        }
+    }
 
-    require('indexView.php');
+    else {
+        
+        listPosts();
+    }
 
