fc9f9cef8ace1f64edfc2692fc8f35023b10c2d2
Link1986 <ali.ghoudi@gmail.com>
2018-01-22 16:07:08 +0100

    Ajout du dossier view/

---
+++ view/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+   
+    <head>
+       
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="css/blog.css" rel="stylesheet" />
+         
+    </head>
+        
+    <body>
+       
+        <?= $content ?>
+        
+    </body>
+    
+</html>
