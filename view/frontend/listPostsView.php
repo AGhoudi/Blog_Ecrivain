4da0c5e6617ed7a17ff46fcc2a05a0a662db4a84
Link1986 <ali.ghoudi@gmail.com>
2018-02-18 22:30:43 +0100

    ajout du template du blog

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -1,11 +1,193 @@
-<?php $title = 'Mon blog'; ?>
+<?php $title = 'Billet simple pour L\'Alaska, le blog de Jean Forteroche'; ?>
 
     <?php ob_start(); ?>
     
-    <h1>Mon super blog !</h1>
+    <!-- Menu de navigation -->
+       
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top mb-5">
     
-    <p>Derniers billets du blog :</p>
-
+        <div class="container">
+    
+            <a class="navbar-brand order-1 mr-0" href="#" >Billet simple pour l'Alaska</a>
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        
+                <span class="navbar-toggler-icon"></span>
+            
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        
+                <div class="navbar-nav">
+            
+                    <a class="nav-item nav-link text-white" href="#">Accueil</a>
+                
+                    <a class="nav-item nav-link text-white" href="#presentation">Présentation</a>
+                
+                    <a class="nav-item nav-link text-white" href="#blog">Blog</a>
+                
+                    <a class="nav-item nav-link text-white" href="#administration">Administration</a>
+                
+                </div>
+            
+            </div>
+        
+        </div>
+        
+    </nav>
+    
+    <!-- Espace d'accueil du blog -->
+    
+    <div class="jumbotron text-sm-center pt-5 mb-0">
+    
+        <h1 class="display-5 my-5">Blog de Jean Forteroche</h1>
+        
+        <p class="lead my-5">Bienvenue cher visiteur sur mon blog, celui-ci me servira à publier mon nouveau roman: "Billet simple pour l'Alaska".
+        </p>
+  
+        <div class="card" style="width: 18rem;">
+        
+            <img class="card-img-top" src="../../../projet4/public/image/logo.jpg" alt="Card image cap">
+            
+            <div class="card-body">
+            
+                <h5 class="card-title">L'Alaska</h5>
+                
+                <p class="card-text text-dark font-weight-bold">L'Alaska est une contrée glacée, se situant en Amérique du nord. Elle est caractérisé par un climat rude.
+                </p>
+    
+            </div>
+            
+        </div>
+  
+        <div class="btn-group my-5 d-flex justify-content-center" role="group" aria-label="buttons">
+            
+                <a class="btn btn-light btn-lg" href="#presentation">Présentation</a>
+                
+                <a class="btn btn-light btn-lg" href="#blog">Blog</a>
+                               
+        </div>
+  
+    </div>
+    
+    <!-- Carrousel sur le thème de l'Alaska -->
+    
+    <div class="container">
+      
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        
+            <ol class="carousel-indicators">
+            
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                
+            </ol>
+            
+        <div class="carousel-inner">
+        
+            <div class="carousel-item active">
+            
+                <img class="d-block w-100 rounded" src="../../../test-blog/public/image/image1.jpg" alt="First slide">
+                
+            </div>
+            
+            <div class="carousel-item">
+            
+                <img class="d-block w-100 rounded" src="../../../test-blog/public/image/image2.jpg" alt="Second slide">
+                
+            </div>
+            
+            <div class="carousel-item">
+            
+                <img class="d-block w-100 rounded" src="../../../test-blog/public/image/image3.jpg" alt="Third slide">
+                
+            </div>
+            
+            <div class="carousel-item">
+            
+                <img class="d-block w-100 rounded" src="../../../test-blog/public/image/image4.jpg" alt="Four slide">
+                
+            </div>
+    
+        </div>
+        
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            
+            <span class="sr-only">Previous</span>
+            
+        </a>
+        
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            
+            <span class="sr-only">Next</span>
+            
+        </a>
+        
+        </div>
+  
+    </div>    
+        
+    <!-- Liste des romans et présentation -->
+    
+    <div class="container" id="presentation">
+    
+        <div class="row">
+        
+            <div class="col-sm-4">
+            
+                <h3 class="my-5">Liste de mes précédents romans</h3>
+                
+                <div class="list-group">
+                
+                    <a class="list-group-item list-group-item-action">Une nuit d'hiver</a>
+                    
+                    <a class="list-group-item list-group-item-action">Le calme avant la tempête</a>
+                    
+                    <a class="list-group-item list-group-item-action">La lutte</a>
+                    
+                    <a class="list-group-item list-group-item-action">Voyage merveilleux</a>
+                    
+                    <a class="list-group-item list-group-item-action">Du plus haut des sommets</a>
+                    
+                    <a class="list-group-item list-group-item-action">Le fond des océans</a>                    
+                    
+                </div>
+                
+            </div>
+            
+            <div class="col-sm-8">
+            
+                <h3 class="my-5 pl-5 text-center">Qui suis-je?</h3>
+                
+                <div class="bg-light rounded">
+                
+                    <p class="text-dark text-center">Je tiens à me présenter, pour ceux qui ne me connaîtrais pas encore. Je suis Jean Forteroche, acteur et écrivain, qui prends plaisir à voyager à travers mes romans, et partager avec mes lecteurs mon goût pour l'aventure</p>
+                
+                    <p class="text-dark text-center">J'adore voyager et découvrir de nouvelles cultures et personnes. Même si j'ai été un acteur pendant des années, je m'investis désormais beaucoup plus dans mes écrits</p>
+                
+                    <p class="text-dark text-center">Ainsi je suis heureux de partager mon nouveau roman "billet simple pour l'Alaska", avec vous cher lecteurs sur mon blog, et je vous souhaite une bonne lecture, amicalement, Jean.</p>
+                
+                </div>
+                
+            </div>
+            
+        </div>
+            
+    </div>
+    
+    <!-- Liste des chapitres et blog -->
+    
+    <h3 class="my-5 pl-5 text-center" id="blog">Billet simple pour l'Alaska</h3>
+    
     <?php
 
         while ($data = $posts->fetch())
@@ -13,27 +195,41 @@
         {
         
     ?>
+   
+    <div class="container">
     
-    <div class="news">
-       
-        <h3>
-           
-            <?= htmlspecialchars($data['title']) ?>
-            
-            <em>le <?= $data['creation_date_fr'] ?></em>
-            
-        </h3>
+        <div class="row">
         
-        <p>
+            <div class="col-sm-12">    
+      
+                <div class="bg-success rounded mb-5">         
+                
+                    <h3 class="text-center text-white chapitre py-2">
            
-            <?= nl2br(htmlspecialchars($data['content'])) ?>
+                        <?= htmlspecialchars($data['title']) ?>            
             
-            <br />
-            
-            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
-            
-        </p>
+                    </h3>
         
+                    <p  class="text-white text-center py-2 border">
+           
+                        <?= nl2br(htmlspecialchars($data['content'])) ?>            
+            
+                    </p>
+                    
+                    <p class="text-white text-center py-2">
+                    
+                        <em>le <?= $data['creation_date_fr'] ?></em>
+                        
+                    </p>
+        
+                    <a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-warning mx-auto d-flex justify-content-center mb-3 font-weight-bold font-italic" style="width: 200px;">Ajouter un commentaire</a>
+                                
+                </div>
+        
+            </div>
+    
+        </div>
+    
     </div>
     
     <?php
@@ -46,4 +242,4 @@
     
     <?php $content = ob_get_clean(); ?>
 
-    <?php require('view/frontend/template.php'); ?>
+    <?php require('template.php'); ?>
--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,55 +1,162 @@
 <?php $title = htmlspecialchars($post['title']); ?>
 
-    <?php ob_start(); ?>
-
-    <h1>Mon super blog !</h1>
+    <?php ob_start(); ?>    
     
-    <p><a href="index.php">Retour à la liste des billets</a></p>
-
-    <div class="news">
-    
-        <h3>
-            <?= htmlspecialchars($post['title']) ?>
-            
-            <em>le <?= $post['creation_date_fr'] ?></em>
-            
-        </h3>
-    
-        <p>
-        
-            <?= nl2br(htmlspecialchars($post['content'])) ?>
-            
-        </p>
-        
-    </div>
-
-    <h2>Commentaires</h2>
-    
-    <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-    
-    <div>
+    <!-- Menu de navigation -->
        
-        <label for="author">Auteur</label><br />
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top mb-5">
+    
+        <div class="container">
+    
+            <a class="navbar-brand order-1 mr-0" href="index.php" >Billet simple pour l'Alaska</a>
         
-        <input type="text" id="author" name="author" />
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        
+                <span class="navbar-toggler-icon"></span>
+            
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        
+                <div class="navbar-nav">
+        
+                    <a class="nav-item nav-link text-white" href="index.php#blog">Retour à la liste des billets</a>                
+                
+                </div>
+            
+            </div>
+        
+        </div>
+        
+    </nav>
+    
+    <!-- Chapitre du blog -->
+    
+    <div class="container">
+    
+        <div class="row">
+        
+            <div class="col-sm-12 my-5">
+            
+                <h3 class="my-5 pl-5 text-center">Billet simple pour l'Alaska</h3>
+                
+                <div class="bg-success rounded">
+    
+                    <h3 class="text-center text-white chapitre py-2">
+                    
+                        <?= htmlspecialchars($post['title']) ?>            
+            
+                    </h3>
+    
+                    <p class="text-white text-center py-2 border">
+        
+                        <?= nl2br(htmlspecialchars($post['content'])) ?>
+            
+                    </p>
+        
+                    <p class="text-white text-center py-2">
+                    
+                        <em>le <?= $post['creation_date_fr'] ?></em>
+                        
+                    </p>
+        
+                </div>
+                
+            </div>
+            
+        </div>
         
     </div>
-    
-    <div>
        
-        <label for="comment">Commentaire</label><br />
-        
-        <textarea id="comment" name="comment"></textarea>
-        
-    </div>
+    <!-- Module de messagerie -->
+            
+    <div class="container">
     
-    <div>
+        <div class="row">
+        
+            <div class="col-sm-12">
+            
+                <h3 class="my-5 pl-5 text-center">Commentaires</h3>
+                
+                <div class="bg-success rounded">
        
-        <input type="submit" />
+                    <form class="col-sm-6 mx-auto" action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+                    
+                        <div class="form-group">
+                        
+                            <label for="author" class="text-white">Pseudo :</label>
+                            
+                            <input type="text" class="form-control" id="author" name="author" aria-describedby="emailHelp">
+                            
+                        </div>
+                        
+                        <div class="form-group">
+                        
+                            <label for="comment" class="text-white">Message :</label>
+                            
+                            <textarea class="form-control" id="comment" name="comment" rows="3"></textarea>
+                            
+                        </div>
+  
+                        <button type="submit" class="btn btn-primary d-flex justify-content-center mb-3">Envoyer</button>
+                        
+                    </form>
+        
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>    
+    
+    <!-- Button trigger modal -->
+    
+    <button type="button" class="btn btn-warning mx-auto d-flex justify-content-center mb-3 font-weight-bold font-italic my-3" data-toggle="modal" data-target="#exampleModal">
+    
+    Signaler un commentaire
+    
+    </button>
+
+    <!-- Modal -->
+    
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    
+        <div class="modal-dialog" role="document">
+        
+            <div class="modal-content">
+            
+                <div class="modal-header">
+                
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    
+                        <span aria-hidden="true">&times;</span>
+                        
+                    </button>
+                    
+                </div>
+                
+                <div class="modal-body">
+                
+                    ...
+                    
+                </div>
+                
+                <div class="modal-footer">
+                
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                    
+                </div>
+                
+            </div>
+            
+        </div>
         
     </div>
-    
-    </form>
 
     <?php
     
@@ -59,16 +166,34 @@
             
     ?>
     
-        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+    <div class="container">
+    
+        <div class="row">
         
-        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+            <div class="col-sm-12">        
+        
+                <div class="bg-info rounded my-1 text-white">
+        
+                    <h3 class="text-center text-white chapitre py-2"><?= htmlspecialchars($comment['author']) ?></h3>
+            
+                    <p class="text-white text-center py-2 border"><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+            
+                    <p class="text-white text-center py-2"><em>le <?= $comment['comment_date_fr'] ?></em></p>        
+        
+                </div>
+            
+            </div>
+            
+        </div>
+        
+    </div>
         
     <?php
         
         }
             
     ?>
-    
+
     <?php $content = ob_get_clean(); ?>
 
-    <?php require('view/frontend/template.php'); ?>
+    <?php require('template.php'); ?>
